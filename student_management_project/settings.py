488ab3f30e11afd398334335a2c3e2616aabb40a
Jay <aadhyainstituteairdm@gmail.com>
2025-01-06 14:07:23 +0530

    update db

--- student_management_project/settings.py
+++ student_management_project/settings.py
@@ -89,15 +89,31 @@ WSGI_APPLICATION = 'student_management_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
         
-    }
+#     }
+# }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'management_gx27',
+#         'USER': 'management_gx27_user',
+#         'PASSWORD': 'ObuV0laEdrDIXIh8OuXk85yU793AlPF1',
+#         'HOST': 'localhost',  # Use the database host, e.g., '127.0.0.1' or a remote server
+#         'PORT': '5432',       # Default PostgreSQL port
+#     }
+# }
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://management_gx27_user:ObuV0laEdrDIXIh8OuXk85yU793AlPF1@dpg-cttp9vggph6c738kug2g-a/management_gx27'
+    )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
